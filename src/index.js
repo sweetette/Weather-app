8babfd02a6a2a623886db9cfb233cd5f7bd3e748
sweetette <sweetette-connolly-dulce@hotmail.com>
2022-04-02 02:50:29 +0200

    JS forecast template

--- src/index.js
+++ src/index.js
@@ -22,6 +22,33 @@ let days = [
 let day = days[now.getDay()];
 date.innerHTML = `${day} ${hours}:${minutes}`;
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+                <div class="weather-forecast-date">${day}</div>
+                <img
+                  src="http://openweathermap.org/img/wn/50d@2x.png"
+                  alt=""
+                  width="42"
+                />
+                <div class="weather-forecast-temperature">
+                  <span class="weather-forecast-temperature-max">18</span>
+                  <span class="weather-forecast-temperature-min">12</span>
+                </div>
+            </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+displayForecast();
 ///
 function search(event) {
   event.preventDefault();
